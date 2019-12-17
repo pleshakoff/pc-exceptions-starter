9fefe329e1eb20a6a252fa55a61dc22b4c712c8b
pleshakoff <pleshakoff@gmail.com>
2019-12-17 18:21:39 +0300

    Конфигурация.

---
+++ src/main/java/com/parcom/ExceptionHandlerConfiguration.java
@@ -0,0 +1,10 @@
+package com.parcom;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.parcom.exceptions.handler")
+public class ExceptionHandlerConfiguration {
+
+}
