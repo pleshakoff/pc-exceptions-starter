1930b86ce41887737d7825ee1494e2690074db02
pleshakoff <pleshakoff@gmail.com>
2019-12-17 19:45:07 +0300

    Добавил новый тип exception

--- src/main/java/com/parcom/exceptions/handler/GlobalDefaultExceptionHandler.java
+++ src/main/java/com/parcom/exceptions/handler/GlobalDefaultExceptionHandler.java
@@ -6,20 +6,37 @@ import com.parcom.exceptions.NotFoundParcomException;
 import com.parcom.exceptions.ParcomException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.ConversionNotSupportedException;
+import org.springframework.beans.TypeMismatchException;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.http.converter.HttpMessageNotWritableException;
+import org.springframework.lang.Nullable;
 import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingPathVariableException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.ServletRequestBindingException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.context.request.async.AsyncRequestTimeoutException;
+import org.springframework.web.multipart.support.MissingServletRequestPartException;
+import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
 import java.nio.file.AccessDeniedException;
+import java.util.List;
 import java.util.Optional;
 
 
@@ -81,19 +98,59 @@ public class GlobalDefaultExceptionHandler extends ResponseEntityExceptionHandle
         return new ResponseEntity<>(getExceptionResourceLocalized(request, ex),HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    @ExceptionHandler(value = Exception.class)
+    @ExceptionHandler(value = BindException.class)
+    public ResponseEntity<Object> handleBindException(HttpServletRequest request, BindException ex)
+    {
+        String message = getMessageFromErrorList(ex.getBindingResult());
+        return new ResponseEntity<>(getExceptionResource(request, ex, message),HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleBindException(HttpServletRequest request,MethodArgumentNotValidException ex)
+    {
+        String message = getMessageFromErrorList(ex.getBindingResult());
+        return new ResponseEntity<>(getExceptionResource(request, ex, message),HttpStatus.BAD_REQUEST);
+    }
+
+
+    private String getMessageFromErrorList(BindingResult bindingResult) {
+        List<ObjectError> allErrors = bindingResult.getAllErrors();
+        StringBuilder mess = new StringBuilder();
+        for (ObjectError error : allErrors) {
+            mess.append(messageSource.getMessage(error, LocaleContextHolder.getLocale())).append("; ");
+        }
+        return mess.toString();
+    }
+
+
+
+
+    @ExceptionHandler({
+            HttpRequestMethodNotSupportedException.class,
+            HttpMediaTypeNotSupportedException.class,
+            HttpMediaTypeNotAcceptableException.class,
+            MissingPathVariableException.class,
+            MissingServletRequestParameterException.class,
+            ServletRequestBindingException.class,
+            ConversionNotSupportedException.class,
+            TypeMismatchException.class,
+            HttpMessageNotReadableException.class,
+            HttpMessageNotWritableException.class,
+            MissingServletRequestPartException.class,
+            NoHandlerFoundException.class,
+            AsyncRequestTimeoutException.class
+    })
+    @Nullable
+    public final ResponseEntity<Object> handleExcluded(Exception ex, WebRequest request) throws Exception {
+        throw ex;
+    }
+
+        @ExceptionHandler(value = Exception.class)
     public ResponseEntity<ExceptionResource> handleException(HttpServletRequest request, Exception ex) {
         return new ResponseEntity<>(getExceptionResource(request, ex),HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    @Override
-    protected ResponseEntity<Object> handleBindException(BindException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        StringBuilder mess = new StringBuilder();
-        for (ObjectError error : ex.getBindingResult().getAllErrors()) {
-            mess.append(messageSource.getMessage(error, LocaleContextHolder.getLocale())).append("; ");
-        }
-        return new ResponseEntity<>(getExceptionResource(null, ex, mess.toString()),HttpStatus.BAD_REQUEST);
-    }
+
 
 
 
